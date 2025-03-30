114af63e34a138e97732b23e67a9fe490ec61f6f
Neel11Naba <nabag353739@gmail.com>
2025-03-30 18:50:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function (event) {
             event.preventDefault();
             const type = this.getAttribute("data-type");
+
+            if (!type) {
+                console.error("Error: data-type is missing for this button!");
+                alert("Error: data-type is missing!");
+                return;
+            }
+
+            console.log("Button Clicked:", type);
             alert("Button Clicked: " + type);
             handleConversion(type);
         });
@@ -30,32 +38,3 @@ function handleConversion(type) {
     
     input.click();
 }
-
-function convertTextToPDF(file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const text = e.target.result;
-        alert("Converting: " + file.name);
-        
-        const { jsPDF } = window.jspdf;
-        let pdf = new jsPDF();
-        pdf.text(text, 10, 10);
-        pdf.save("converted.pdf");
-        alert("Download Started!");
-    };
-    reader.readAsText(file);
-}
-
-function convertImageToPDF(file) {
-    const { jsPDF } = window.jspdf;
-    let pdf = new jsPDF();
-    let img = new Image();
-    img.src = URL.createObjectURL(file);
-    
-    img.onload = function () {
-        alert("Image Loaded: " + file.name);
-        pdf.addImage(img, 'JPEG', 10, 10, 180, 160);
-        pdf.save("converted.pdf");
-        alert("Download Started!");
-    };
-            }
